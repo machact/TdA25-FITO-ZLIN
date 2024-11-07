8c2d295219b941abd97c90cf21f532dc13835381
fifa-s <svoboda.11filip11@gmail.com>
2024-11-07 17:34:12 +0100

    game route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{middleware::Logger, web, App, HttpServer, Responder};
+use actix_web::{middleware, web, App, HttpServer, Responder};
 use actix_files::{Files, NamedFile};
 use log::info;
 mod api;
@@ -7,6 +7,7 @@ mod api;
 async fn not_found() -> impl Responder {
     NamedFile::open("static/404.html")
 }
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -17,11 +18,16 @@ async fn main() -> std::io::Result<()> {
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     HttpServer::new(move || {
-        let logger = Logger::default();
+        let logger = middleware::Logger::default();
         App::new()
-            .route("/api", web::get().to(api::hello_api))
-            .service(Files::new("/", "static/root/").index_file("index.html"))
-            .default_service(web::route().to(not_found))
             .wrap(logger)
+            .default_service(web::route().to(not_found))
+            .route("/api", web::get().to(api::hello_api))
+            .service(
+                Files::new("/game/", "static/game/")
+                    .index_file("index.html")
+                    .redirect_to_slash_directory()
+            )
+            .service(Files::new("/", "static/root/").index_file("index.html"))
     }).bind(format!("0.0.0.0:{port}"))?.run().await
 }
