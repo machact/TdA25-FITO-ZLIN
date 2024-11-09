3ff266f415890fc48e85029dd53728e676fa4159
fifa-s <svoboda.11filip11@gmail.com>
2024-11-09 23:27:44 +0100

    /games post and get

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{middleware, web, App, HttpServer, Responder};
 use actix_files::{Files, NamedFile};
 use sqlx::sqlite::{SqlitePool, SqliteConnectOptions};
-use log::info;
+use log::{info, error};
 use std::env;
 
 mod api;
@@ -12,19 +12,19 @@ async fn not_found() -> impl Responder {
 }
 
 async fn setup_db(pool: &SqlitePool) -> Result<(), sqlx::Error> {
-    let create_table_query = r#"
-        CREATE TABLE IF NOT EXISTS games (
-            uuid TEXT PRIMARY KEY,
-            createdAt TEXT NOT NULL,
-            uploadedAt TEXT NOT NULL,
-            name TEXT,
-            difficulty TEXT NOT NULL,
-            gameState TEXT,
-            board TEXT NOT NULL
-        );
-    "#;
-
-    sqlx::query(create_table_query)
+    sqlx::query(
+        r#"
+            CREATE TABLE IF NOT EXISTS games (
+                uuid TEXT PRIMARY KEY,
+                createdAt TEXT NOT NULL,
+                uploadedAt TEXT NOT NULL,
+                name TEXT NOT NULL,
+                difficulty TEXT NOT NULL,
+                gameState TEXT NOT NULL,
+                board TEXT NOT NULL
+            );
+        "#
+    )
         .execute(pool)
         .await?;
     
@@ -32,13 +32,20 @@ async fn setup_db(pool: &SqlitePool) -> Result<(), sqlx::Error> {
 }
 
 
+
+
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = dotenvy::from_filename(".env");
+    if let Err(_) = dotenvy::from_filename(".env") {
+        error!("Couldn't find .env file.")
+    }
     env_logger::init();
 
+    let database_path = "sqlite:app.db";
+    info!("DATABASE_URL: {}", database_path);
+
     let options = SqliteConnectOptions::new()
-        .filename(&env::var("DATABASE_URL").expect("Enviroment variable DATABASE_URL must be set"))
+        .filename(database_path)
         .create_if_missing(true);
 
     let pool = SqlitePool::connect_with(options).await?;
@@ -54,10 +61,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .app_data(web::Data::new(pool.clone()))
             .wrap(logger)
             .default_service(web::route().to(not_found))
+            .route("/api/v1/games", web::get().to(api::games_get))
+            .route("/api/v1/games", web::post().to(api::games_post))
             .route("/api", web::get().to(api::hello_api))
             .service(
                 Files::new("/game/", "static/game/")
-                    .index_file("index.html")
+                    .index_file("game.html")
                     .redirect_to_slash_directory()
             )
             .service(Files::new("/", "static/root/").index_file("index.html"))
