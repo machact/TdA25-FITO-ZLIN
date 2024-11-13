87d3c1f14fc17efee89dbc7b50dadf6a3242f669
fifa-s <svoboda.11filip11@gmail.com>
2024-11-13 22:46:56 +0100

    correct getting uuid from path

--- src/api/games_uuid.rs
+++ src/api/games_uuid.rs
@@ -2,10 +2,10 @@ use actix_web::{web, HttpResponse};
 use sqlx::SqlitePool;
 use super::api_utils::GameError;
 
-pub async fn delete(pool: web::Data<SqlitePool>, query: web::Query<Option<String>>) -> Result<HttpResponse, GameError> {
+pub async fn delete(pool: web::Data<SqlitePool>, uuid: web::Path<String>) -> Result<HttpResponse, GameError> {
     todo!()
 }
 
-pub async fn put(pool: web::Data<SqlitePool>, query: web::Query<Option<String>>) -> Result<HttpResponse, GameError> {
+pub async fn put(pool: web::Data<SqlitePool>, uuid: web::Path<String>) -> Result<HttpResponse, GameError> {
     todo!()
 }
